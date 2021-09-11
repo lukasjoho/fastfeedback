ec57adb68df8226ad520f92aa84e52ae9a0f80b9
lukasjoho <social@lukashoppe.com>
2021-09-11 23:27:59 +0200

    Add chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import { CSSReset, ChakraProvider } from '@chakra-ui/react';
+import { AuthProvider } from '../lib/auth';
+import customTheme from '../styles/theme';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import { Global, css } from '@emotion/react';
 
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 350px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
